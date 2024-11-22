ccd90d8e4726fd4f0f0bcc56cf073ae93d36b756
rupokghosh <rupokghosh291@gmail.com>
2024-11-21 20:20:11 -0800

    fix: debug auth bugs

--- model.php
+++ model.php
@@ -2,15 +2,15 @@
 
 $conn = mysqli_connect("localhost", "w3radin", "w3radin136", "C354_w3radin");
 function signUpUser($username, $email, $password) {
-    global $db;
+    global $conn;
     $query = "INSERT INTO User_Details (username, email, password) VALUES ('$username', '$email', '$password')";
-    return mysqli_query($db, $query);
+    return mysqli_query($conn, $query);
 }
 
 function loginUser($email, $password) {
-    global $db;
+    global $conn;
     $query = "SELECT username FROM User_Details WHERE email='$email' AND password='$password'";
-    $result = mysqli_query($db, $query);
+    $result = mysqli_query($conn, $query);
     return mysqli_num_rows($result) > 0 ? mysqli_fetch_assoc($result) : null;
 }
 ?>
